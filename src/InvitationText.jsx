4e7309507c8b181c575b0301bbac6116290d088e
Ashot Mkrtchyan <ashot.mkrtchyan@bostongene.com>
2025-07-30 14:46:33 +0400

    fixes wording

--- src/InvitationText.jsx
+++ src/InvitationText.jsx
@@ -2,7 +2,7 @@ function InvitationText() {
   return (
     <div className="invitation-text-container">
       <div className="vrdznagir">
-        Սիրով հրավիրում ենք ձեզ կիսելու մեզ հետ մեր կյանքի կարևոր և հիշարժան օրը
+        Սիրով հրավիրում ենք Ձեզ կիսելու մեզ հետ մեր կյանքի կարևոր և հիշարժան օրը
       </div>
       <div className="vrdznagir">Սպասում ենք ձեզ մեր հարսանիքին</div>
       <div className="vrdznagir">05 սեպտեմբերի 2025</div>
